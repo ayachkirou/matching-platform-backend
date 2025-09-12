0428f83eb71bf04c7627d0bcd57b6300696ed740
Aya chkirou <ayachkiro@gmail.com>
2025-09-12 15:32:16 +0100

    Ajout PostgreSQL, configuration datasource et dependencies

---
+++ src/main/java/net/java/guides/matching/entity/Test.java
@@ -0,0 +1,12 @@
+package net.java.guides.matching.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Test {
+@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
