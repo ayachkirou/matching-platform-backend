55467f9cd563288f252d54280b2fdb75904fc17f
aya <ayaaatfaoui38@gmail.com>
2025-10-17 13:26:35 +0100

    commit

--- src/main/java/net/java/guides/matching/controllers/OffreController.java
+++ src/main/java/net/java/guides/matching/controllers/OffreController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/offres")
 @CrossOrigin(origins = "http://localhost:5173")
-public class OffreController {
+public class OffreController{
 
     @Autowired
     private OffreService offreService;
